7abe494d837018f72ef1d313129c7674d40d1559
Teszt Elek <you@inf.unideb.hu>
2024-10-07 11:12:23 +0200

    DAO interface added

---
+++ src/main/java/hu/unideb/inf/JPAAnimalDAO.java
@@ -0,0 +1,25 @@
+package hu.unideb.inf;
+
+import java.util.List;
+
+public class JPAAnimalDAO implements AnimalDAO{
+    @Override
+    public void saveAnimal(Animal a) {
+
+    }
+
+    @Override
+    public List<Animal> getAllAnimals() {
+        return List.of();
+    }
+
+    @Override
+    public void updateAnimal(Animal a) {
+
+    }
+
+    @Override
+    public void deleteAnimal(Animal a) {
+
+    }
+}
